4fde2dc121349ebaf97ec4041d363aadcd1b3f1f
starxd1222 <wuslvosrpwjd@gmail.com>
2025-07-09 11:00:18 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
+    <div style={{ maxWidth: 600, margin: 'auto', padding: 20 }}>
       <h1>쪄니위키에 오신 걸 환영합니다!</h1>
-      <p>이곳에서 위키 문서를 확인할 수 있어요.</p>
+      <p>이곳에서 쪄니위키 문서를 확인할 수 있어요.</p>
     </div>
   )
 }
